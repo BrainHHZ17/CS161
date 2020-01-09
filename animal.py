2aa27f8c67a6bd094810152efaf298fb6e003433
BrainHHZ17 <59673470+BrainHHZ17@users.noreply.github.com>
2020-01-08 20:04:20 -0800

    Create animal.py

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Hanhui Zeng
+# Date: 1/8/2020
+# Description: Asks the user for their animal and then
+#              prints out that this is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
